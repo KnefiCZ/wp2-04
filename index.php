618cb5afddafe5d29942482d6f664e685fed642a
Matěj Kneifl <matej.kneifl@seznam.cz>
2018-11-13 11:49:02 +0100

    ouuuuuuuuuuuuuu(::(:(:

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="cs" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>kok</title>
+  </head>
+  <body>
+
+  <?php
+
+
+  for ($i=0; $i < 30 ; $i++) {
+  $age=rand(0,30);
+    if ($age >= 18) {
+      echo $age . "Pojď dále.";
+    }
+  }
+
+    ?>
+  </body>
+  </html>
